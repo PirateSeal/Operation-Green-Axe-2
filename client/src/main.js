4e51289f95d1620ac3f245a3441b2a3b1e3a91f2
PirateSeal <tcousin.pseal@gmail.com>
2020-07-28 19:39:03 +0200

    Added bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,7 @@
 import Vue from 'vue';
+
+import 'bootswatch/dist/slate/bootstrap.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="jumbotron">
+    <h1 class="display-3">Welcome to Operation Green Axe 2 website</h1>
+    <p class="lead">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, praesentium numquam, earum
+      quam illum fuga possimus quis eos quod magni obcaecati commodi, ex nemo reiciendis veniam
+      mollitia officia fugit inventore.
+    </p>
+    <hr class="my-4" />
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum tempora, quia ad eos magni
+      aspernatur saepe distinctio ea, sit animi autem ex excepturi repellendus a corrupti iste
+      dolore provident alias.
+    </p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">U SUK</a>
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
